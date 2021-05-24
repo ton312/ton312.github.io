9d3406b130ccb01f51825050e4d6e29594764d67
Alex <lesha-noskov@mail.ru>
2021-05-24 21:35:53 +0300

    new_commit3

--- lb/repairs_wagon_project/repairs_wagon_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RepairsWagonsAppConfig(AppConfig):
-    name = 'repairs_wagon_app'
--- lb/repairs_wagon_project/repairs_wagon_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import redirect_blog
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', redirect_blog),
-    path('admin/', admin.site.urls),
-    path('', include('repairs_wagon_app.urls')),
-]
